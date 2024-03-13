26981e823a17c06226f0cafdce13505dcc97e605
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-03-13 18:00:50 +0100

    installing styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Button variation="primary" size="medium">
+          Check In
+        </Button>
+        <Button variation="secondary" size="small">
+          Check Out
+        </Button>
+        <Input type="number" />
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;

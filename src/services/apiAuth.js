74708a665582a8e3481277240efbf78573ee1cf4
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-04-26 15:57:18 +0200

    implementing login

---
+++ src/services/apiAuth.js
@@ -0,0 +1,27 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  //console.log(data);
+  return data;
+}
+
+//getting current user (if there is user and is it still authenticated)
+export async function getCurrentUser() {
+  //checking is there active session
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}

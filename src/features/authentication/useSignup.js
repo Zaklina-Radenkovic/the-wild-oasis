e1648a864f908cf986b0af842bfd04136d7e374d
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-04-29 11:35:13 +0200

    clearing code and updating it not to make newly signuped user as a current user

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,23 +1,15 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { signup as signupApi } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useSignup() {
-  // const queryClient = useQueryClient();
-  // const navigate = useNavigate();
-
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: (user) => {
-      console.log(user);
+      //console.log(user);
       toast.success(
         "Account successfully created! Please, verify the new account from the user's email address"
       );
-      // navigate("/dashboard", { replace: true });
-    },
-    onError: (err) => {
-      console.log("ERROR", err);
     },
   });
 
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,53 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
+//////sign up
+export async function signup({ fullName, email, password }) {
+  // Save the current session before signing up a new user
+  const { data: savedSessionData } = await supabase.auth.getSession();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  // Log the entire response for debugging
+  //console.log("Sign-up response:", { user, error });
+
+  //If there was a previously authenticated user, restore their session
+  // This action should be placed right after signUp, otherwise the authError will stop the restore
+  if (savedSessionData) {
+    await supabase.auth.setSession(savedSessionData.session);
+  }
+
+  // Handle errors
+  let authError = null;
+  if (user && !user.identities.length) {
+    authError = {
+      name: "AuthApiError",
+      message: "This email has already been registered",
+    };
+  } else if (error) {
+    authError = {
+      name: error.name,
+      message: error.message,
+    };
+  }
+
+  if (authError) throw new Error(authError.message);
+
+  return user;
+}
+
+/////log in
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -12,7 +60,7 @@ export async function login({ email, password }) {
   return data;
 }
 
-//getting current user (if there is user and is it still authenticated)
+//////getting current user (if there is user and is it still authenticated)
 export async function getCurrentUser() {
   //checking is there active session
   const { data: session } = await supabase.auth.getSession();
@@ -26,25 +74,40 @@ export async function getCurrentUser() {
   return data?.user;
 }
 
-//logout
+///////logout
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
 }
 
-//sign up
-export async function signup({ fullName, email, password }) {
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      data: {
-        fullName,
-        avatar: "",
-      },
+////update current user
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  //1. update password or fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  //2. upload avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  //3. update avatar in the user
+  const { data: updateUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
     },
   });
-  if (error) throw new Error(error.message);
 
-  return data;
+  if (error2) throw new Error(error2.message);
+  return updateUser;
 }

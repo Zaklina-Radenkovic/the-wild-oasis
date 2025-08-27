05762b48ef7cf8c20831395bc0e63efbe62a9540
Zaklina Radenkovic <zradenkovic@hotmail.com>
2025-08-27 21:43:08 +0200

    Fix supabase connection env vars

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ygzdfmdqrttszxvtnyxj.supabase.co";
+export const supabaseUrl = "https://acdzrhbhdvytsbfvyvyp.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnemRmbWRxcnR0c3p4dnRueXhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0Mzk3MjIsImV4cCI6MjAyNjAxNTcyMn0.bBoYbG2BdWkDFchGCchpi8eIFxP3PMGq43a7SY-fypA";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjZHpyaGJoZHZ5dHNiZnZ5dnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDAwMDEsImV4cCI6MjA3MTg3NjAwMX0.z3EDIstQ5okEa-0lt0Udr6Lx9xa-CNCrR7jNaZTcEwk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
